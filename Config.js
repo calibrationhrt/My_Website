acb3ae0b328587d8f880a32affb8c4361a05b48d
Calibration <calibrationhrt@gmail.com>
2026-04-20 16:42:37 +0700

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCI0J3GHKFl9lmVwOM03stAxwvHHkYFHEM",
+  authDomain: "calibration-bcc66.firebaseapp.com",
+  projectId: "calibration-bcc66",
+  storageBucket: "calibration-bcc66.firebasestorage.app",
+  messagingSenderId: "156162245790",
+  appId: "1:156162245790:web:09d71d12511176410d278f",
+  measurementId: "G-EK23GYH3FL"
+};
+
+console.log("Hello");
